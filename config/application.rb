69600f758b6215b3bd413c3e7bdaf50286aa0ee4
Egan Bernardino <eegun19@yahoo.com>
2016-08-03 13:15:29 -0700

    deleted migration drop_table and also added config.assets to application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module MusicSheetApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %w(#{config.root}/app/models/ckeditor)
   end
 end
--- db/migrate/20160725183638_drop_tablename.rb
+++
@@ -1,5 +0,0 @@
-class DropTablename < ActiveRecord::Migration
-  def change
-    drop_table :music_sheets
-  end
-end
